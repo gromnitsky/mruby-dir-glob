ebcf11b2a2300d53a26ebe91922edfcc68e2701b
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2014-08-29 14:34:40 +0300

    port Dir.glob from rubinius

---
+++ dir.rb
@@ -0,0 +1,30 @@
+require [File.dirname(__FILE__), './dir_glob'].join '/'
+
+class Dir
+
+  def self.glob(pattern, flags=0, &block)
+    if pattern.kind_of? Array
+      patterns = pattern
+    else
+      return [] if pattern.empty?
+      patterns = [pattern]
+    end
+
+    matches = []
+    index = 0
+
+    patterns.each do |pat|
+      Dir::Glob.glob pat, flags, matches
+
+      index += matches.size
+    end
+
+    if block
+      matches.each(&block)
+      return nil
+    end
+
+    return matches
+  end
+
+end
